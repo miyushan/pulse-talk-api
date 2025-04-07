cda003f209ddc6b3f05858d4a2218ff895c1d5ed
miyushan <miyushanrodrigo@gmail.com>
2025-04-07 12:25:41 +0530

    fix: avoid using config service inside main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-
   app.enableCors({
-    origin: configService.get<string>('CLIENT_URL'),
+    origin: process.env.CLIENT_URL,
     credentials: true,
   });
   app.use(cookieParser());
